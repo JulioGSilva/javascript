abe190a0922f8f27517087d7b4372796088e9849
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2022-12-15 12:48:52 -0300

    att aula 012

--- aulas/aula012ex/script.js
+++ aulas/aula012ex/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+//alert('Olá')//
+var h = new Date()
+var hora = h.getHours()
+var min = h.getMinutes()
+var sec = h.getSeconds()
+var r = window.document.getElementById('res')
+r.innerHTML = `${hora}:${min}:${sec}`
+
+//menssagem de 'são ${horas}' boa ${noite}//
+
+if (hora >= 6 && hora <= 12) {
+    var cor = window.document.querySelector('body')
+    cor.style.background = '#dbc4a4'
+} else if(hora >= 13 && hora <= 17) {
+    cor.style.background = '#bd8661'
+} else if(hora > 17 && hora <= 23) {
+    cor.style.background = '#4b4688'
+} else if(hora == 0 && hora < 6) {
+    cor.style.background = '#4b4688'
+}
