fb6e6b432ed1652dc283fa9930f615c49a5b39bc
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2022-12-18 10:50:38 -0300

    up

--- aulas/aula012ex/script.js
+++ aulas/aula012ex/script.js
@@ -1,20 +1,20 @@
 //alert('Olá')//
-var h = new Date()
-var hora = h.getHours()
-var min = h.getMinutes()
-var sec = h.getSeconds()
-var r = window.document.getElementById('res')
-r.innerHTML = `Olá!, são ${hora} horas e ${min} minutos`
 
-//menssagem de 'são ${horas}' boa ${noite}//
-
-if (hora >= 6 && hora <= 12) {
-    var cor = window.document.querySelector('body')
-    cor.style.background = '#dbc4a4'
-} else if(hora >= 13 && hora <= 17) {
-    cor.style.background = '#bd8661'
-} else if(hora > 17 && hora <= 23) {
-    cor.style.background = '#4b4688'
-} else if(hora == 0 && hora < 6) {
-    cor.style.background = '#4b4688'
-}
+    var data = new Date()
+    var hora = data.getHours()
+    var img = window.document.getElementById('imagem')
+    var msg = window.document.getElementById('res')
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <= 12) {
+        //DIA
+        img.src = 'Imagens/manhã.jpg'
+        window.document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        //tarde
+        img.src = 'Imagens/tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        //noite
+        img.src = 'Imagens/noite.jpg'
+        document.body.style.background = '#515154'
+    }
