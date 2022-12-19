f5a39466f520f9e1044e43e3825778ee93c5ef73
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2022-12-19 18:54:39 -0300

    js

--- aulas/aula012ex/script.js
+++ aulas/aula012ex/script.js
@@ -1,15 +1,16 @@
-//alert('Olá')//
-var h = new Date()
-var hora = h.getHours()
-var min = h.getMinutes()
-var sec = h.getSeconds()
-var r = window.document.getElementById('res')
-r.innerHTML = `${hora}:${min}:${sec}`
+//alert('Olá')
+var h = new Date();
+var hora = h.getHours();
+var min = h.getMinutes();
+var sec = h.getSeconds();
+var r = window.document.getElementById('res');
+var cor = window.document.querySelector('body')
+r.innerHTML = `Olá!, são ${hora} horas`
 
 //menssagem de 'são ${horas}' boa ${noite}//
 
+
 if (hora >= 6 && hora <= 12) {
-    var cor = window.document.querySelector('body')
     cor.style.background = '#dbc4a4'
 } else if(hora >= 13 && hora <= 17) {
     cor.style.background = '#bd8661'
