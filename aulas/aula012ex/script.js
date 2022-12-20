e500c7a022990a734131beebb7a3f3042e3a526a
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2022-12-19 22:00:04 -0300

    exercicios finalizados

--- aulas/aula012ex/script.js
+++ aulas/aula012ex/script.js
@@ -1,21 +1,26 @@
-//alert('Olá')
-var h = new Date();
-var hora = h.getHours();
-var min = h.getMinutes();
-var sec = h.getSeconds();
-var r = window.document.getElementById('res');
-var cor = window.document.querySelector('body')
-r.innerHTML = `Olá!, são ${hora} horas`
-
-//menssagem de 'são ${horas}' boa ${noite}//
-
-
-if (hora >= 6 && hora <= 12) {
-    cor.style.background = '#dbc4a4'
-} else if(hora >= 13 && hora <= 17) {
-    cor.style.background = '#bd8661'
-} else if(hora > 17 && hora <= 23) {
-    cor.style.background = '#4b4688'
-} else if(hora == 0 && hora < 6) {
-    cor.style.background = '#4b4688'
+function carregar() {
+        //alert('Olá')
+        var h = new Date();
+        var hora = h.getHours();
+        var r = window.document.getElementById('res');
+        var cor = window.document.querySelector('body')
+        var img = document.getElementById('imagem')
+        r.innerHTML = `Olá!, são ${hora} horas`
+    
+        //menssagem de 'são ${horas}' boa ${noite}//
+    
+    
+        if (hora >= 6 && hora <= 12) {
+            cor.style.background = '#dbc4a4'
+            img.src = 'imagens/manhã.jpg'
+        } else if(hora >= 13 && hora <= 17) {
+            cor.style.background = '#bd8661'
+            img.src = 'imagens/tarde.jpg'
+        } else if(hora > 17 && hora <= 23) {
+            cor.style.background = '#4b4688'
+            img.src = 'imagens/noite.jpg'
+        } else if(hora == 0 && hora < 6) {
+            cor.style.background = '#4b4688'
+            img.src = 'imagens/noite2.png'
+        }    
 }
