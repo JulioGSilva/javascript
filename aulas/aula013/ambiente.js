f134a1bb41942c84cff037229d9975a097b412b6
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2022-12-20 22:01:16 -0300

    innicio ex14

--- aulas/aula013/ambiente.js
+++ aulas/aula013/ambiente.js
@@ -1,1 +1,11 @@
-console.log('Tudo bem?')
+var cont = 1
+
+do {
+    console.log(`passo ${cont}`)
+    cont++
+} while (cont <= 10)
+
+/*while (cont <=6 ) {
+    console.log(`O contador está em ${cont}`)
+    cont += 1
+}*/
