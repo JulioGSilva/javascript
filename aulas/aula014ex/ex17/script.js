318114a0ce6018563e7370593eea2a9bf9dc36b5
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2022-12-23 22:31:46 -0300

    fim ex17

--- aulas/aula014ex/ex17/script.js
+++ aulas/aula014ex/ex17/script.js
@@ -1,11 +1,29 @@
 function tabuada() {
     let num = document.getElementById('num')
-    let n1 = Number(num.value)
-    let res = document.getElementById('res')
-    let c = 1
-    while(c <= 10) {
+    let tab = document.getElementById('tabu')
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n1 = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let op = document.createElement('option')
+            op.text = `${n1} X ${c} = ${n1*c}`
+            op.value = `tab${c}`
+            tab.appendChild(op)
+            c++
+        }
+    }
+
+
+
+
+
+    /*while(c <= 10) {
         let r = n1 * c
         res.innerHTML += `${r} - `
         c += 1
-    }
+
+    }*/
 }
