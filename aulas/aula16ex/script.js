bced3843714a6ae2854ffa7ca5b65327ca5af9ac
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2022-12-26 22:24:45 -0300

    up ex16

--- aulas/aula16ex/script.js
+++ aulas/aula16ex/script.js
@@ -1,17 +1,39 @@
+let num = document.getElementById('num').value
+let lista = document.getElementById('lista')
 let res = document.getElementById('res')
-let lista = []
+let valores = []
+
+function isNumero(n) {
+    if ( Array.includes(n)) {
+        return true
+    } else {
+        return false
+    }
+   /*if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+   } else {
+        return false
+   }*/
+}
+
+function inlista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function adicionar() {
-    let num = document.getElementById('num').value
-    let n = parseInt(num)
-    let tab = document.getElementById('sele')
-    let item = document.createElement('option')
-    item.setAttribute('id', 'opt')
-    item.innerText = `Valor ${num} adicionado`
-    tab.appendChild(item)
-    lista.push(n)
+    if(!isNaN(num) && !inlista(num.value, valores)) {
+        alert('ok')
+    } else {
+        window.alert('Valor invalido')
+    }
     
 }
+
+
 
 function finalizar() {
     let max = Math.max.apply(null, lista)
@@ -22,7 +44,7 @@ function finalizar() {
     }
     let t = lista.length
     let media = soma / t
-    res.innerHTML += `Ao todo, temos ${lista.length} números cadastrados <br><br>
+    res.innerHTML = `Ao todo, temos ${lista.length} números cadastrados <br><br>
     O maior valor informado foi ${max} <br><br>
     O menor valor informado foi ${min} <br><br>
     A soma dos valores é ${soma} <br><br>
