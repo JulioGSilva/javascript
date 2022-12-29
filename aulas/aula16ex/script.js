5dbb79275a86d95a6982ddbdf2d6dba0a6585e2e
JulioGSilva <113954423+JulioGSilva@users.noreply.github.com>
2022-12-28 21:44:11 -0300

    javaScript Fim

--- aulas/aula16ex/script.js
+++ aulas/aula16ex/script.js
@@ -1,19 +1,14 @@
-let num = document.getElementById('num').value
+let num = document.getElementById('num')
 let lista = document.getElementById('lista')
 let res = document.getElementById('res')
 let valores = []
 
 function isNumero(n) {
-    if ( Array.includes(n)) {
-        return true
-    } else {
-        return false
-    }
-   /*if (Number(n) >= 1 && Number(n) <= 100) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
    } else {
         return false
-   }*/
+   }
 }
 
 function inlista(n, l) {
@@ -25,18 +20,51 @@ function inlista(n, l) {
 }
 
 function adicionar() {
-    if(!isNaN(num) && !inlista(num.value, valores)) {
-        alert('ok')
+    if(isNumero(num.value) && !inlista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
-        window.alert('Valor invalido')
+        window.alert('Valor invalido, ou já encontrado na lista')
     }
+    num.value = ''
+    num.focus()
     
 }
 
 
 
 function finalizar() {
-    let max = Math.max.apply(null, lista)
+    if (valores.length == 0) {
+        alert('Adicione valoeres para finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores
+            
+            if (valores[pos] > maior) {
+                maior =  valores[pos]
+            } else if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao total, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior} e o menor informado valor foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `A media dos valores é ${media}`
+    }
+    
+    
+
+    /*let max = Math.max.apply(null, lista)
     let min = Math.min.apply(null, lista)
     let soma = 0
     for(let c = 0; c < lista.length; c++){
